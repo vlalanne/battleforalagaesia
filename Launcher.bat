5d595b679d70920c2c49c6ae2a05f9c0eb8c209c
Vincent Lalanne <vlalanne90@gmail.com>
2013-05-07 17:03:08 +0000

    Bug fixes - 07/05/2013

--- Launcher.bat
+++ Launcher.bat
@@ -1,3 +1,7 @@
 set MODFOLDER="%~dp0."
+IF "%PROGRAMFILES(x86)%"=="" (
 set ROTKLAUNCHER="%PROGRAMFILES%\Electronic Arts\The Lord of the Rings, The Rise of the Witch-king\lotrbfme2ep1.exe"
+) ELSE (
+set ROTKLAUNCHER="%PROGRAMFILES(x86)%\Electronic Arts\The Lord of the Rings, The Rise of the Witch-king\lotrbfme2ep1.exe"
+)
 %ROTKLAUNCHER% -mod %MODFOLDER%
--- Launcher_x64.bat
+++
@@ -1,3 +0,0 @@
-set MODFOLDER="%~dp0."
-set ROTKLAUNCHER="%PROGRAMFILES(x86)%\Electronic Arts\The Lord of the Rings, The Rise of the Witch-king\lotrbfme2ep1.exe"
-%ROTKLAUNCHER% -mod %MODFOLDER%
